3c57c7c96ad2bc26804d1c4cfd3c3edce967cf9f
guopf <626138959@qq.com>
2020-03-24 18:13:57 +0800

    chore: update

--- packages/mom-cli/lib/create.js
+++ packages/mom-cli/lib/create.js
@@ -2,7 +2,8 @@ const path = require("path");
 const fse = require("fs-extra");
 
 module.exports = function(targetDir, options) {
-  const templateDir = path.resolve(process.env.MO_CLI_HOME, "./mo");
+  const templateDir = require("mom").resolve();
+  // const templateDir = path.resolve(process.env.MO_CLI_HOME, "./mo");
   console.log("初始化项目...");
   fse.copy(templateDir, targetDir)
       .then(() => console.log(`项目创建成功: ${targetDir}`))
--- packages/mom-cli/lib/generator.js
+++ packages/mom-cli/lib/generator.js
@@ -2,7 +2,8 @@ const path = require("path");
 const fse = require("fs-extra");
 
 module.exports = function(distDir, options) {
-  const templateDir = path.resolve(process.env.MO_CLI_HOME, "./code-tpl");
+  const templateDir = require("mom-tpl").resolve();
+  // const templateDir = path.resolve(process.env.MO_CLI_HOME, "./code-tpl");
   const generator = new Generator(templateDir, distDir, options);
 
   if (options.module) {
