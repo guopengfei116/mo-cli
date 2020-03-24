18e2011c6d2cc885eaf65b8d5d145e2e7bdc45b5
guopf <626138959@qq.com>
2020-03-25 03:24:22 +0800

    chore: mo-quick dir

--- packages/mo-quick-cli/lib/create.js
+++ packages/mo-quick-cli/lib/create.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fse = require("fs-extra");
 
 module.exports = function(targetDir, options) {
-  const templateDir = path.resolve(process.env.MO_CLI_HOME, "./node_modules/mo-quick");
+  const templateDir = path.dirname(require.resolve("mo-quick"));
 
   console.log("初始化项目...");
   fse.copy(templateDir, targetDir)
